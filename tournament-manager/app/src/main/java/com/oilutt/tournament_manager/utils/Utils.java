5f9acf725aff79d471d1a57407513afa8bd2acba
Túlio Henrique <oilutt@hotmail.com>
2017-09-26 08:43:34 -0300

    Update Utils
    PrintKeyHash

--- tournament-manager/app/src/main/java/com/oilutt/tournament_manager/utils/Utils.java
+++ tournament-manager/app/src/main/java/com/oilutt/tournament_manager/utils/Utils.java
@@ -1022,4 +1022,37 @@ public class Utils {
         Random random = new Random();
         return random.nextInt(99);
     }
+    
+    public static String printKeyHash(Activity context) {
+        PackageInfo packageInfo;
+        String key = null;
+        try {
+            //getting application package name, as defined in manifest
+            String packageName = context.getApplicationContext().getPackageName();
+
+            //Retriving package info
+            packageInfo = context.getPackageManager().getPackageInfo(packageName,
+                    PackageManager.GET_SIGNATURES);
+
+            Log.e("Package Name=", context.getApplicationContext().getPackageName());
+
+            for (Signature signature : packageInfo.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                key = new String(Base64.encode(md.digest(), 0));
+
+                // String key = new String(Base64.encodeBytes(md.digest()));
+                Log.e("Key Hash=", key);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("Name not found", e1.toString());
+        }
+        catch (NoSuchAlgorithmException e) {
+            Log.e("No such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("Exception", e.toString());
+        }
+
+        return key;
+    }
 }
